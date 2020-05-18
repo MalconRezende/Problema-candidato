dfb96f65f859c53591b07a69511a6c2dbce37b8b
Malcon Rezende <rezendemalcon13@gmail.com>
2020-05-18 13:30:49 -0300

    Leitura dos dados

--- main.cpp
+++ main.cpp
@@ -4,6 +4,18 @@ using namespace std;
 
 int main()
 {
-    cout << "Hello world!" << endl;
+    double RAS, RAPS, RAGC, GM, GE;
+
+    cout << "Renda anual com salario: " <<endl;
+    cin >> RAS;
+    cout << "Renda anual com salario: " <<endl;
+    cin >> RAPS;
+    cout << "Renda anual com ganho de capital: " <<endl;
+    cin >> RAGC;
+    cout << "Gastos medicos: " <<endl;
+    cin >> GM;
+    cout << "Gastos educacionais: " <<endl;
+    cin >> GE;
+
     return 0;
 }
