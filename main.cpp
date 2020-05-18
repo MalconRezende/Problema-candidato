4d7a48f35b9b2a50b10d45445fb1203b12c0344c
Malcon Rezende <rezendemalcon13@gmail.com>
2020-05-18 16:20:03 -0300

    Consolidado de renda

--- main.cpp
+++ main.cpp
@@ -1,21 +1,47 @@
-#include <iostream>
+#include <bits/stdc++.h>
 
 using namespace std;
 
 int main()
 {
     double RAS, RAPS, RAGC, GM, GE;
+    double IPS, ISS, ISGC;
 
-    cout << "Renda anual com salario: " <<endl;
+    cout << "Renda anual com salario: ";
     cin >> RAS;
-    cout << "Renda anual com salario: " <<endl;
+    cout << "Renda anual com prestacao de servico: ";
     cin >> RAPS;
-    cout << "Renda anual com ganho de capital: " <<endl;
+    cout << "Renda anual com ganho de capital: ";
     cin >> RAGC;
-    cout << "Gastos medicos: " <<endl;
+    cout << "Gastos medicos: ";
     cin >> GM;
-    cout << "Gastos educacionais: " <<endl;
+    cout << "Gastos educacionais: ";
     cin >> GE;
+
+
+    double SM = RAS / 12.0;
+
+    if (SM < 3000){
+        IPS = 0;
+    }
+    else if (SM < 5000) {
+        IPS = RAS * 0.1;
+    }
+    else {
+        IPS = RAS * 0.2;
+    }
+
+    ISS = RAPS * 0.15;
+    ISGC = RAGC * 0.2;
+
+
+    cout <<endl<< "RELATORIO DE IMPOSTO DE RENDA" <<endl<<endl;
+
+    cout << fixed << setprecision(2);
+    cout << "CONSOLIDADO DE RENDA: " <<endl;
+    cout << "Imposto sobre salario: " << IPS <<endl;
+    cout << "Imposto sobre servicos: " << ISS <<endl;
+    cout << "Imposto sobre ganho de capital: " << ISGC <<endl;
 
     return 0;
 }
